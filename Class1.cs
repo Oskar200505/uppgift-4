3a5d94458346d727830470059dd12e694514e879
Oskar200505 <oskhag050521@student.jenseneducation.se>
2022-11-11 09:14:27 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_4
+{
+    internal class Djur
+    {
+
+        private string namn;
+        private string ålder;
+        public string getNamn()
+        {
+            return this.namn;
+        }
+        public void setNamn(string namn)
+        {
+            this.namn = namn;
+        }
+        public class Hund : Djur
+        {
+            private int svansenslängd;
+
+            public int getSvansenslängd()
+            {
+                return this.svansenslängd;
+            }
+
+            public void setsvansenslängd(int svansenslängd)
+            {
+                this.svansenslängd = svansenslängd;
+            }
+       
+            public void skäll()
+            {
+                Console.WriteLine("rrrrufffh");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using uppgift_4;
+
+void Djur()
+{
+    Djur.Hund hund = new Djur.Hund();
+    hund.setNamn("Ubbe");
+    Console.WriteLine("namn:)" + hund.getNamn());
+    hund.setsvansenslängd(23);
+    Console.WriteLine("Längd på svansen:)" + hund.getSvansenslängd());
+    hund.skäll();
+}
+Djur();
